5d524f4489d7188743cedead160aff3a2abfc45a
CallumMoseley <callum.moseley1@gmail.com>
2016-04-06 11:54:32 -0400

    Initial commit.

---
+++ Scanner Project/src/LaserScannerTest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class LaserScannerTest {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		while (true) {
+			System.out.println(s.nextLine());
+		}
+	}
+}
